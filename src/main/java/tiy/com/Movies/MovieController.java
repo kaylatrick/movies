57cfc6a2822cd2a3e639fb1edd7c863bcf74a6bc
john ulmer <john.ulmer@libertymutual.com>
2017-06-27 13:03:49 -0400

    updates to movie controller

--- src/main/java/tiy/com/Movies/MovieController.java
+++ src/main/java/tiy/com/Movies/MovieController.java
@@ -1,5 +1,19 @@
 package tiy.com.Movies;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping('movie')
 public class MovieController {
+	@RequestMapping(path = "/movie", method = RequestMethod.GET)
+	public String movie(Model model, String title, 
+			String runtime, int year, String plotSummary, Genre genre) {
+			Movies m = new Movies(title, runtime, year, plotSummary, genre);
+			model.addAttribute("movie", m);
+			return "movie";
+	}
 
 }

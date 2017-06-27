492e281a79b9473ed4eab3554da57bea17629fcc
Romano <n0252325@lm.lmig.com>
2017-06-27 10:47:17 -0400

    init commit

---
+++ src/main/java/tiy/com/Movies/Movies.java
@@ -0,0 +1,12 @@
+package tiy.com.Movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movies {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movies.class, args);
+	}
+}

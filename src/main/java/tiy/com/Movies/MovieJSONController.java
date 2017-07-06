6b4ed5f75a150f7ca640f4664da36ce14211aaeb
john ulmer <john.ulmer@libertymutual.com>
2017-07-06 15:04:27 -0400

    with tester class

--- src/main/java/tiy/com/Movies/MovieJSONController.java
+++ src/main/java/tiy/com/Movies/MovieJSONController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc; 
 
 @RestController
+@EnableWebMvc
 public class MovieJSONController {
 	@Autowired
 	private MovieRepository movieRepository;
@@ -68,7 +70,7 @@ public class MovieJSONController {
 	public List<Review> jsonAllReviews() {
 		List<Review> reviewList = new ArrayList<Review>();
 		reviewList = reviewRepository.findAll();
-		Movie m = movieRepository.getOne(3);
+		//Movie m = movieRepository.getOne(3);
 		//reviewList = reviewRepository.findAll(Movie m);
 		return reviewList;
 	}
